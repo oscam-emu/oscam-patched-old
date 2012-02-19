oscam-emu/oscam-patched-old

commit 19d2d5977befef208ff1d63c5c5b97209afeeb8f

ATR is stored twice in s_reader, removing one
git-svn-id: https://svn.streamboard.tv/oscam/trunk@6420 4b0bc96b-bc66-0410-9d44-ebda105a78c1

csctapi/icc_async.c
@@ -390,7 +390,7 @@ int32_t ICC_Async_Activate (struct s_reader *reader, ATR * atr, uint16_t depreca
 	ATR_GetRaw(atr, atrarr, &atr_size);
 	char tmp[atr_size*3+1];
 	cs_ri_log(reader, "ATR: %s", cs_hexdump(1, atrarr, atr_size, tmp, sizeof(tmp)));
-	memcpy(reader->card_atr, atrarr, atr_size);
+	memcpy(reader->atr, atrarr, atr_size);
 	reader->card_atr_length = atr_size;
 
 	/* Get ICC reader->convention */

csctapi/ifd_pcsc.c
@@ -172,7 +172,7 @@ int32_t pcsc_activate_card(struct s_reader *pcsc_reader, uchar *atr, uint16_t *a
         pcsc_reader->init_history_pos=0;
 
         cs_log("%s ATR: %s",pcsc_reader->label, cs_hexdump(1, (uchar *)pbAtr, dwAtrLen, tmp, sizeof(tmp)));
-    	memcpy(pcsc_reader->card_atr, pbAtr, dwAtrLen);
+    	memcpy(pcsc_reader->atr, pbAtr, dwAtrLen);
     	pcsc_reader->card_atr_length = dwAtrLen;
         return(1);
     }

globals.h
@@ -1115,7 +1115,6 @@ struct s_reader  									//contains device info, reader info and card info
 	int8_t			force_irdeto;
 	uchar			rsa_mod[120];					// rsa modulus for nagra cards.
 	uchar			atr[64];
-	uchar			card_atr[64];					// ATR readed from card
 	int8_t			card_atr_length;				// length of ATR
 	int32_t			atrlen;
 	SIDTABBITS		sidtabok;						// positiv services

oscam-http.c
@@ -2762,7 +2762,7 @@ static char *send_oscam_entitlement(struct templatevars *vars, struct uriparams 
 				}
 
 				if(rdr->card_atr_length)
-					for(i = 0; i < rdr->card_atr_length; i++) tpl_printf(vars, TPLAPPEND, "READERATR", "%02X ", rdr->card_atr[i]);
+					for(i = 0; i < rdr->card_atr_length; i++) tpl_printf(vars, TPLAPPEND, "READERATR", "%02X ", rdr->atr[i]);
 
 				tpl_addVar(vars, TPLADD, "READERCSYSTEM", rdr->csystem.desc ? rdr->csystem.desc : "unknown");
 

commit 25c3cf3a2311daa98c1b1e6c7b3472cfbeefb5a4

Fixed ins7e11 fast reset broken by r6420
git-svn-id: https://svn.streamboard.tv/oscam/trunk@6424 4b0bc96b-bc66-0410-9d44-ebda105a78c1

csctapi/icc_async.c
@@ -307,7 +307,7 @@ int32_t ICC_Async_Activate (struct s_reader *reader, ATR * atr, uint16_t depreca
 
 	reader->current_baudrate = DEFAULT_BAUDRATE; //this is needed for all readers to calculate work_etu for timings
 
-	if (reader->atr[0] != 0) {
+	if (reader->atr[0] != 0 && !reader->ins7e11_fast_reset) {
 		cs_log("using ATR from reader config");
 		ATR_InitFromArray(atr, reader->atr, ATR_MAX_SIZE);
 	}