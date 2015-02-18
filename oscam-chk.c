oscam-emu/oscam-patched-old

commit 47c8a5b8f587368e2a44651403f893d36eb44d87

Convert caidtab to dynarray.
Less code, smaller data structures, smaller binary size and added
tests for the functionality.

{{{
 Structures:
   struct s_caidtab | -176
   struct s_reader  | -168
   struct s_client  | -176
   struct s_auth    | -176
   struct s_config  | -520

 Binary:
  add/remove: 3/1 grow/shrink: 10/10 up/down: 577/-1679 (-1102)
}}}

caidtab is used by the following config settings:
 ACCOUNT: caid
  READER: caid
  GLOBAL: lb_noproviderforcaid
  GLOBAL: double_check_caid
  GLOBAL: cwcycle_check_caid

Now each of theses can hold more than 32 values which was the old
hard coded limit.

git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@10557 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam-chk.c
@@ -543,17 +543,18 @@ static int32_t chk_rfilter(ECM_REQUEST *er, struct s_reader *rdr)
 
 int32_t chk_ctab(uint16_t caid, CAIDTAB *ctab)
 {
-	if(!caid || !ctab->caid[0])
+	if(!caid || !ctab->ctnum)
 		{ return 1; }
 
 	int32_t i;
-	for(i = 0; i < CS_MAXCAIDTAB; i++)
+	for(i = 0; i < ctab->ctnum; i++)
 	{
-		if(!ctab->caid[i])
+		CAIDTAB_DATA *d = &ctab->ctdata[i];
+		if(!d->caid)
 		{
 			return 0;
 		}
-		if((caid & ctab->mask[i]) == ctab->caid[i])
+		if((caid & d->mask) == d->caid)
 			{ return 1; }
 	}
 	return 0;
@@ -561,17 +562,18 @@ int32_t chk_ctab(uint16_t caid, CAIDTAB *ctab)
 
 int32_t chk_ctab_ex(uint16_t caid, CAIDTAB *ctab)
 {
-	if(!caid || !ctab->caid[0])
+	if(!caid || !ctab->ctnum)
 		{ return 0; }
 
 	int32_t i;
-	for(i = 0; i < CS_MAXCAIDTAB; i++)
+	for(i = 0; i < ctab->ctnum; i++)
 	{
-		if(!ctab->caid[i])
+		CAIDTAB_DATA *d = &ctab->ctdata[i];
+		if(!d->caid)
 		{
 			return 0;
 		}
-		if((caid & ctab->mask[i]) == ctab->caid[i])
+		if((caid & d->mask) == d->caid)
 			{ return 1; }
 	}
 	return 0;
@@ -949,11 +951,14 @@ int32_t matching_reader(ECM_REQUEST *er, struct s_reader *rdr)
 
 int32_t chk_caid(uint16_t caid, CAIDTAB *ctab)
 {
-	int32_t n, rc;
-	for(rc = -1, n = 0; (n < CS_MAXCAIDTAB) && (rc < 0); n++)
-		if((caid & ctab->mask[n]) == ctab->caid[n])
-			{ rc = ctab->cmap[n] ? ctab->cmap[n] : caid; }
-	return rc;
+	int32_t i;
+	for(i = 0; i < ctab->ctnum; i++)
+	{
+		CAIDTAB_DATA *d = &ctab->ctdata[i];
+		if((caid & d->mask) == d->caid)
+			return d->cmap ? d->cmap : caid;
+	}
+	return -1;
 }
 
 int32_t chk_caid_rdr(struct s_reader *rdr, uint16_t caid)

globals.h
@@ -556,11 +556,17 @@ typedef struct s_classtab
 	uchar           bclass[31];
 } CLASSTAB;
 
+typedef struct s_caidtab_data
+{
+	uint16_t		caid;
+	uint16_t		mask;
+	uint16_t		cmap;
+} CAIDTAB_DATA;
+
 typedef struct s_caidtab
 {
-	uint16_t        caid[CS_MAXCAIDTAB];
-	uint16_t        mask[CS_MAXCAIDTAB];
-	uint16_t        cmap[CS_MAXCAIDTAB];
+	int32_t			ctnum;
+	CAIDTAB_DATA	*ctdata;
 } CAIDTAB;
 
 typedef struct s_tuntab_data

module-cccshare.c
@@ -1284,13 +1284,14 @@ void update_card_list(void)
 
 			if((rdr->typ != R_CCCAM) && !rdr->caid && !flt)
 			{
-				for(j = 0; j < CS_MAXCAIDTAB; j++)
+				for(j = 0; j < rdr->ctab.ctnum; j++)
 				{
-					//cs_log("CAID map CCcam card report caid: %04X cmap: %04X", rdr->ctab.caid[j], rdr->ctab.cmap[j]);
-					uint16_t lcaid = rdr->ctab.caid[j];
+					CAIDTAB_DATA *d = &rdr->ctab.ctdata[j];
+					//cs_log("CAID map CCcam card report caid: %04X cmap: %04X", d->caid, d->cmap);
+					uint16_t lcaid = d->caid;
 
 					if(!lcaid || (lcaid == 0xFFFF))
-						{ lcaid = rdr->ctab.cmap[j]; }
+						{ lcaid = d->cmap; }
 
 					if(lcaid && (lcaid != 0xFFFF))
 					{
@@ -1308,15 +1309,16 @@ void update_card_list(void)
 				}
 			}
 
-			if((rdr->typ != R_CCCAM) && rdr->ctab.caid[0] && !flt)
+			if((rdr->typ != R_CCCAM) && rdr->ctab.ctnum && !flt)
 			{
 				//cs_log("tcp_connected: %d card_status: %d ", rdr->tcp_connected, rdr->card_status);
 				int32_t c;
 				if(rdr->tcp_connected || rdr->card_status == CARD_INSERTED)
 				{
-					for(c = 0; c < CS_MAXCAIDTAB; c++)
+					for(c = 0; c < rdr->ctab.ctnum; c++)
 					{
-						uint16_t caid = rdr->ctab.caid[c];
+						CAIDTAB_DATA *d = &rdr->ctab.ctdata[c];
+						uint16_t caid = d->caid;
 						if(!caid) { break; }
 
 						card = create_card2(rdr, c, caid, reshare);

module-monitor.c
@@ -743,7 +743,7 @@ static void monitor_set_account(char *args)
 				break;     //betatunnel
 
 			case    8:
-				clear_caidtab(&account->ctab);
+				caidtab_clear(&account->ctab);
 				break;    //Caid
 			}
 			found = i;

module-newcamd.c
@@ -616,10 +616,10 @@ static void mk_user_ftab(FILTER *filt)
 
 	// 1. CAID
 	// search server CAID in client CAID
-	for(c = i = 0; i < CS_MAXCAIDTAB; i++)
+	for(c = i = 0; i < cl->ctab.ctnum; i++)
 	{
-		int32_t ctab_caid;
-		ctab_caid = cl->ctab.caid[i] & cl->ctab.mask[i];
+		CAIDTAB_DATA *d = &cl->ctab.ctdata[i];
+		int32_t ctab_caid = d->caid & d->mask;
 		if(ctab_caid) { c++; }
 
 		if(psfilt->caid == ctab_caid)

module-scam.c
@@ -824,8 +824,9 @@ static void scam_caidlist_add(uint16_t *caidlist, uint32_t listsize, uint32_t *c
 static void scam_server_send_caidlist(struct s_client *cl)
 {
 	uchar mbuf[5];
-	uint32_t i = 0, j = 0;
-	uint16_t caid = 0, caids[55];
+	int32_t j;
+	uint32_t i = 0;
+	uint16_t caids[55];
 	uint32_t cardcount = 0;
 	struct s_reader *rdr = NULL;
 	
@@ -833,19 +834,19 @@ static void scam_server_send_caidlist(struct s_client *cl)
 	for(rdr = first_active_reader; rdr; rdr = rdr->next)
 	{
 		if(rdr->caid && chk_ctab(rdr->caid, &cl->ctab)) {
-			scam_caidlist_add(caids, 55, &cardcount, rdr->caid);
+			scam_caidlist_add(caids, ARRAY_SIZE(caids), &cardcount, rdr->caid);
 		}
 		
-		for(j=0; j<CS_MAXCAIDTAB; j++) {
-			caid = rdr->ctab.caid[j];
-			if(caid && chk_ctab(caid, &cl->ctab)) {
-				scam_caidlist_add(caids, 55, &cardcount, caid);
-			}			
-		}	
+		for(j = 0; j < rdr->ctab.ctnum; j++) {
+			CAIDTAB_DATA *d = &rdr->ctab.ctdata[j];
+			if(d->caid && chk_ctab(d->caid, &cl->ctab)) {
+				scam_caidlist_add(caids, ARRAY_SIZE(caids), &cardcount, d->caid);
+			}
+		}
 	}
 	cs_readunlock(&readerlist_lock);
 
-	for(j=0; j<cardcount; j++) {
+	for(j=0; j < (int32_t)cardcount; j++) {
 		i = 0;
 		mbuf[i++] = 0x20; // caid	data type
 		mbuf[i++] = 0x03; // length

module-stat.c
@@ -59,9 +59,10 @@ void init_stat(void)
 static uint32_t get_prid(uint16_t caid, uint32_t prid)
 {
 	int32_t i;
-	for(i = 0; i < CS_MAXCAIDTAB; i++)
+	for(i = 0; i < cfg.lb_noproviderforcaid.ctnum; i++)
 	{
-		uint16_t tcaid = cfg.lb_noproviderforcaid.caid[i];
+		CAIDTAB_DATA *d = &cfg.lb_noproviderforcaid.ctdata[i];
+		uint16_t tcaid = d->caid;
 		if(!tcaid) { break; }
 		if((tcaid == caid) || (tcaid < 0x0100 && (caid >> 8) == tcaid))
 		{

oscam-array.c
@@ -67,5 +67,6 @@ DECLARE_ARRAY_FUNCS(tuntab, TUNTAB, TUNTAB_DATA, ttdata, ttnum); // Declare tunt
 DECLARE_ARRAY_FUNCS(ecm_whitelist, ECM_WHITELIST, ECM_WHITELIST_DATA, ewdata, ewnum); // Declare ecm_whitelist_clear(), ecm_whitelist_clone(), ecm_whitelist_add()
 DECLARE_ARRAY_FUNCS(ecm_hdr_whitelist, ECM_HDR_WHITELIST, ECM_HDR_WHITELIST_DATA, ehdata, ehnum); // Declare ecm_hdr_whitelist_clear(), ecm_hdr_whitelist_clone(), ecm_hdr_whitelist_add()
 DECLARE_ARRAY_FUNCS(caidvaluetab, CAIDVALUETAB, CAIDVALUETAB_DATA, cvdata, cvnum); // Declare caidvaluetab_clear(), caidvaluetab_clone(), caidvaluetab_add()
+DECLARE_ARRAY_FUNCS(caidtab, CAIDTAB, CAIDTAB_DATA, ctdata, ctnum); // Declare caidtab_clear(), caidtab_clone(), caidtab_add()
 
 #undef DECLARE_ARRAY_FUNCS

commit 0ccbf14b6902b8a5401d91620b0148c7bc122dca

Fix regression introduced in r10557 (try 2).
This reverts r10162 and should fix fixes regression introduced
in r10557.

r10562 was not the right fix. The right fix was to return the
caid when the list is empty.

This should fix ticket #4123 and #4142.

git-svn-id: https://svn.streamboard.tv/oscam/trunk@10563 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam-chk.c
@@ -951,14 +951,15 @@ int32_t matching_reader(ECM_REQUEST *er, struct s_reader *rdr)
 
 int32_t chk_caid(uint16_t caid, CAIDTAB *ctab)
 {
-	int32_t n, rc;
-	for(rc = -1, n = 0; (n < ctab->ctnum) && (rc < 0); n++)
+	int32_t i;
+	if (!ctab->ctnum) return caid;
+	for(i = 0; i < ctab->ctnum; i++)
 	{
-		CAIDTAB_DATA *d = &ctab->ctdata[n];
+		CAIDTAB_DATA *d = &ctab->ctdata[i];
 		if((caid & d->mask) == d->caid)
-			{ rc = d->cmap ? d->cmap : caid; }
+			return d->cmap ? d->cmap : caid;
 	}
-	return rc;
+	return -1;
 }
 
 int32_t chk_caid_rdr(struct s_reader *rdr, uint16_t caid)