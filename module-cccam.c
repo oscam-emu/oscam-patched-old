oscam-emu/oscam-patched-old

commit 3501557c27663bf9b1d000990256ac1ae0899654

fixed buffer overflows when sending/receiving ecms/emms
the rules for ecms are now:
min. size: 0 or >=3 (a size of 1 or 2 is not allowed)
max. size: MAX_ECM_SIZE (defined in globals.h)
SCT_LEN(ecm) must be <= er->ecmlen

the rules for emms are now:
min. size: 0 (we could add also a check for >=3 here, but I am not sure atm whether this would affect emm reassembly)
max. size: MAX_EMM_SIZE (defined in globals.h)



git-svn-id: https://svn.streamboard.tv/oscam/trunk@10812 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-cccam.c
@@ -2058,13 +2058,17 @@ void cc_idle(void)
 	}
 }
 
-struct cc_card *read_card(uint8_t *buf, int32_t ext)
+struct cc_card *read_card(uint8_t *buf, int32_t buflen, int32_t ext)
 {
 	struct cc_card *card;
-	if(!cs_malloc(&card, sizeof(struct cc_card)))
+	int16_t nprov, nassign = 0, nreject = 0;
+	int32_t offset = 21;
+
+	if(buflen < 21)
 		{ return NULL; }
 
-	int32_t nprov, nassign = 0, nreject = 0, offset = 21;
+	if(!cs_malloc(&card, sizeof(struct cc_card)))
+		{ return NULL; }
 
 	card->providers = ll_create("providers");
 	card->badsids = ll_create("badsids");
@@ -2087,13 +2091,19 @@ struct cc_card *read_card(uint8_t *buf, int32_t ext)
 
 	if(ext)
 	{
+		if(buflen < 23)
+			{ cc_free_card(card); return NULL; }
+		
 		nassign = buf[21];
 		nreject = buf[22];
 
 		offset += 2;
 	}
-
-	int32_t i;
+	
+	if(buflen < (offset + (nprov*7)))
+		{ cc_free_card(card); return NULL; }
+	
+	int16_t i;
 	for(i = 0; i < nprov; i++)    // providers
 	{
 		struct cc_provider *prov;
@@ -2110,13 +2120,14 @@ struct cc_card *read_card(uint8_t *buf, int32_t ext)
 		offset += 7;
 	}
 
-	uint8_t *ptr = buf + offset;
-
 	if(ext)
 	{
+		if(buflen < (offset + (nassign*2) + (nreject*2)))
+			{ cc_free_card(card); return NULL; }
+		
 		for(i = 0; i < nassign; i++)
 		{
-			uint16_t sid = b2i(2, ptr);
+			uint16_t sid = b2i(2, buf + offset);
 			//cs_log_dbg(D_CLIENT, "      assigned sid = %04X, added to good sid list", sid);
 
 			struct cc_srvid *srvid;
@@ -2126,12 +2137,12 @@ struct cc_card *read_card(uint8_t *buf, int32_t ext)
 			srvid->chid = 0;
 			srvid->ecmlen = 0;
 			ll_append(card->goodsids, srvid);
-			ptr += 2;
+			offset += 2;
 		}
 
 		for(i = 0; i < nreject; i++)
 		{
-			uint16_t sid = b2i(2, ptr);
+			uint16_t sid = b2i(2, buf + offset);
 			//cs_log_dbg(D_CLIENT, "      rejected sid = %04X, added to sid block list", sid);
 
 			struct cc_srvid_block *srvid;
@@ -2142,20 +2153,24 @@ struct cc_card *read_card(uint8_t *buf, int32_t ext)
 			srvid->ecmlen = 0;
 			srvid->blocked_till = 0;
 			ll_append(card->badsids, srvid);
-			ptr += 2;
+			offset += 2;
 		}
 	}
 
-	int32_t remote_count = ptr[0];
-	ptr++;
+	int16_t remote_count = buf[offset];
+	offset++;
+	
+	if(buflen < (offset + (remote_count*8)))
+		{ cc_free_card(card); return NULL; }
+		
 	for(i = 0; i < remote_count; i++)
 	{
 		uint8_t *remote_node;
 		if(!cs_malloc(&remote_node, 8))
 			{ break; }
-		memcpy(remote_node, ptr, 8);
+		memcpy(remote_node, buf + offset, 8);
 		ll_append(card->remote_nodes, remote_node);
-		ptr += 8;
+		offset += 8;
 	}
 	return card;
 }
@@ -2273,6 +2288,10 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 	cs_log_dbg(cl->typ == 'c' ? D_CLIENT : D_READER, "%s parse_msg=%d", getprefix(), buf[1]);
 
 	uint8_t *data = buf + 4;
+	
+	if(l < 4)
+		{ return -1; }
+	
 	memcpy(&cc->receive_buffer, data, l - 4);
 	cc->last_msg = buf[1];
 	switch(buf[1])
@@ -2405,6 +2424,9 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 	case MSG_NEW_CARD_SIDINFO:
 	case MSG_NEW_CARD:
 	{
+		if(l < 16)
+			{ break; }
+		
 		uint16_t caid = b2i(2, buf + 12);
 		//filter caid==0 and maxhop:
 		if(!caid || buf[14] >= rdr->cc_maxhops + 1)
@@ -2422,7 +2444,7 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 		rdr->card_status = CARD_INSERTED;
 
 		cs_writelock(&cc->cards_busy);
-		struct cc_card *card = read_card(data, buf[1] == MSG_NEW_CARD_SIDINFO);
+		struct cc_card *card = read_card(data, l - 4, buf[1] == MSG_NEW_CARD_SIDINFO);
 		if(!card)
 		{
 			cs_writeunlock(&cc->cards_busy);
@@ -2500,6 +2522,8 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 
 	case MSG_CARD_REMOVED:
 	{
+		if(l < 8)
+			{ break; }
 		cs_writelock(&cc->cards_busy);
 		cc_card_removed(cl, b2i(4, buf + 4));
 		cs_writeunlock(&cc->cards_busy);
@@ -2508,6 +2532,9 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 
 	case MSG_SLEEPSEND:
 		//Server sends SLEEPSEND:
+		if(l < 5)
+			{ break; }
+		
 		if(!cfg.c35_suppresscmd08)
 		{
 			if(buf[4] == 0xFF)
@@ -2526,36 +2553,23 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 		}
 		//NO BREAK!! NOK Handling needed!
 
-	case MSG_CACHE_PUSH:
-	{
-		if((l - 4) >= 18)
-		{
-			cc_cacheex_push_in(cl, data);
-		}
-		break;
-	}
-	
-	case MSG_CACHE_FILTER:
-	{
-		if((l - 4) >= 482)
-		{
-			cc_cacheex_filter_in(cl, data);
-		}
-		break;
-	}	
-
 	case MSG_CW_NOK1:
 	case MSG_CW_NOK2:
+		if(l < 2)
+			{ break; }
+		
 		if(l > 5)
 		{
 			//Received NOK with payload:
 			char *msg = (char *) buf + 4;
 
 			//Check for PARTNER connection:
-			if(strncmp(msg, "PARTNER:", 8) == 0)
+			if((l >= (4+8)) && strncmp(msg, "PARTNER:", 8) == 0)
 			{
 				//When Data starts with "PARTNER:" we have an Oscam-cccam-compatible client/server!
-
+				if((int32_t)(4 + 9 + sizeof(cc->remote_oscam) - 1) > l)
+					{ break; }
+				
 				strncpy(cc->remote_oscam, msg + 9, sizeof(cc->remote_oscam) - 1);
 				int32_t has_param = check_extended_mode(cl, msg);
 				if(!cc->is_oscam_cccam)
@@ -2734,20 +2748,43 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 
 		cc_send_ecm(cl, NULL);
 		break;
+		
+	case MSG_CACHE_PUSH:
+	{
+		if((l - 4) >= 18)
+		{
+			cc_cacheex_push_in(cl, data);
+		}
+		break;
+	}
+	
+	case MSG_CACHE_FILTER:
+	{
+		if((l - 4) >= 482)
+		{
+			cc_cacheex_filter_in(cl, data);
+		}
+		break;
+	}
 
 	case MSG_CW_ECM:
 		cc->just_logged_in = 0;
 		if(cl->typ == 'c')    //SERVER:
 		{
-			ECM_REQUEST *er;
+#define CCMSG_HEADER_LEN 17
 
+			ECM_REQUEST *er;
 			struct cc_card *server_card;
+			
+			if(l < CCMSG_HEADER_LEN)
+				{ break; }
+			
 			if(!cs_malloc(&server_card, sizeof(struct cc_card)))
 				{ break; }
 			server_card->id = buf[10] << 24 | buf[11] << 16 | buf[12] << 8
 							  | buf[13];
 			server_card->caid = b2i(2, data);
-#define CCMSG_HEADER_LEN 17
+
 			if((er = get_ecmtask()) && l > CCMSG_HEADER_LEN && MAX_ECM_SIZE > l - CCMSG_HEADER_LEN)
 			{
 				er->caid = b2i(2, buf + 4);
@@ -2849,6 +2886,9 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 		}
 		else     //READER:
 		{
+			if(l < 20)
+				{ break; }
+			
 			cs_readlock(&cc->cards_busy);
 			cc->recv_ecmtask = -1;
 			eei = get_extended_ecm_idx(cl,
@@ -2984,7 +3024,9 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 		{
 			cc->just_logged_in = 0;
 			l = l - 4;//Header Length=4 Byte
-
+			if(l < 0)
+				{ break; }
+			
 			cs_log_dbg(D_READER, "%s MSG_CMD_05 recvd, payload length=%d mode=%d",
 						  getprefix(), l, cc->cmd05_mode);
 			cc->cmd05_active = 1;
@@ -2996,6 +3038,9 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 		break;
 	case MSG_CMD_0B:
 	{
+		if(l < 20)
+			{ break; }
+		
 		// by Project:Keynation
 		cs_log_dbg(D_READER, "%s MSG_CMD_0B received (payload=%d)!",
 					  getprefix(), l - 4);
@@ -3023,6 +3068,8 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 	case MSG_CMD_0C:   //New CCCAM 2.2.0 Server/Client fake check!
 	{
 		int32_t len = l - 4;
+		if(len < 0)
+			{ break; }
 
 		if(cl->typ == 'c')    //Only im comming from "client"
 		{
@@ -3107,6 +3154,9 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 	case MSG_CMD_0D:   //key update for the active cmd0x0c algo
 	{
 		int32_t len = l - 4;
+		if(len < 0)
+			{ break; }
+		
 		if(cc->cmd0c_mode == MODE_CMD_0x0C_NONE)
 			{ break; }
 
@@ -3120,6 +3170,9 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 
 	case MSG_CMD_0E:
 	{
+		if(l < 2)
+			{ break; }
+		
 		cs_log_dbg(D_READER, "cccam 2.2.x commands not implemented: 0x%02X", buf[1]);
 		//Unkwon commands...need workout algo
 		if(cl->typ == 'c')  //client connection
@@ -3143,42 +3196,47 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 		if(cl->typ == 'c')    //EMM Request received
 		{
 			cc_cmd_send(cl, NULL, 0, MSG_EMM_ACK); //Send back ACK
-			if(l > 4)
+			
+			if(l < 16)
+				{ break; } 
+
+			cs_log_dbg(D_EMM, "%s EMM Request received!", getprefix());
+
+			if(!ll_count(cl->aureader_list))
 			{
-				cs_log_dbg(D_EMM, "%s EMM Request received!", getprefix());
-
-				if(!ll_count(cl->aureader_list))
-				{
-					cs_log_dbg(
-						D_EMM,
-						"%s EMM Request discarded because au is not assigned to an reader!",
-						getprefix());
-					return MSG_EMM_ACK;
-				}
-
-				EMM_PACKET *emm;
-				if(!cs_malloc(&emm, sizeof(EMM_PACKET)))
-					{ break; }
-				emm->caid[0] = buf[4];
-				emm->caid[1] = buf[5];
-				emm->provid[0] = buf[7];
-				emm->provid[1] = buf[8];
-				emm->provid[2] = buf[9];
-				emm->provid[3] = buf[10];
-				//emm->hexserial[0] = buf[11];
-				//emm->hexserial[1] = buf[12];
-				//emm->hexserial[2] = buf[13];
-				//emm->hexserial[3] = buf[14];
-				if(l <= 0xFF)
-					{ emm->emmlen = buf[15]; }
-				else
-					{ emm->emmlen = MIN(l - 16, (int32_t)sizeof(emm->emm)); }
-				memcpy(emm->emm, buf + 16, emm->emmlen);
-				//emm->type = UNKNOWN;
-				//emm->cidx = cs_idx;
-				do_emm(cl, emm);
-				NULLFREE(emm);
+				cs_log_dbg(
+					D_EMM,
+					"%s EMM Request discarded because au is not assigned to an reader!",
+					getprefix());
+				return MSG_EMM_ACK;
 			}
+
+			EMM_PACKET *emm;
+			if(!cs_malloc(&emm, sizeof(EMM_PACKET)))
+				{ break; }
+			emm->caid[0] = buf[4];
+			emm->caid[1] = buf[5];
+			emm->provid[0] = buf[7];
+			emm->provid[1] = buf[8];
+			emm->provid[2] = buf[9];
+			emm->provid[3] = buf[10];
+			//emm->hexserial[0] = buf[11];
+			//emm->hexserial[1] = buf[12];
+			//emm->hexserial[2] = buf[13];
+			//emm->hexserial[3] = buf[14];
+			if(l <= 0xFF)
+				{ emm->emmlen = buf[15]; }
+			else
+				{ emm->emmlen = MIN(l - 16, (int32_t)sizeof(emm->emm)); }
+			
+			if(emm->emmlen < 0 || emm->emmlen > MAX_EMM_SIZE || emm->emmlen+16 > l)
+				{ NULLFREE(emm); break;}
+				
+			memcpy(emm->emm, buf + 16, emm->emmlen);
+			//emm->type = UNKNOWN;
+			//emm->cidx = cs_idx;
+			do_emm(cl, emm);
+			NULLFREE(emm);
 		}
 		else     //Our EMM Request Ack!
 		{

globals.h
@@ -259,6 +259,9 @@ typedef unsigned char uchar;
 #define CS_DELAY          0
 #define CS_ECM_RINGBUFFER_MAX 0x10 // max size for ECM last responsetimes ringbuffer. Keep this set to power of 2 values!
 
+#define MAX_ECM_SIZE 596
+#define MAX_EMM_SIZE 258
+
 #ifndef PTHREAD_STACK_MIN
 #define PTHREAD_STACK_MIN 64000
 #endif
@@ -664,7 +667,7 @@ struct s_emmcache
 	uchar			emmd5[CS_EMMSTORESIZE];
 	uchar			type;
 	uchar			len;
-	uchar			emm[258];
+	uchar			emm[MAX_EMM_SIZE];
 	struct timeb    firstseen;
 	struct timeb    lastseen;
 };
@@ -715,6 +718,8 @@ struct ecm_request_t ;
 struct emm_packet_t ;
 struct s_ecm_answer ;
 struct demux_s ;
+
+#define DEFAULT_MODULE_BUFSIZE 1024
 
 struct s_module
 {
@@ -831,8 +836,6 @@ struct s_cardsystem
 	int32_t (*get_emm_filter)(struct s_reader *, struct s_csystem_emm_filter **, unsigned int *);
 	int32_t (*get_tunemm_filter)(struct s_reader *, struct s_csystem_emm_filter **, unsigned int *);
 };
-
-#define MAX_ECM_SIZE 596
 
 typedef struct ecm_request_t
 {
@@ -2010,10 +2013,9 @@ typedef struct cs_stat_query
 	int16_t         ecmlen;
 } STAT_QUERY;
 
-
 typedef struct emm_packet_t
 {
-	uchar           emm[258];
+	uchar           emm[MAX_EMM_SIZE];
 	int16_t         emmlen;
 	uchar           caid[2];
 	uchar           provid[4];

module-camd33.c
@@ -117,7 +117,7 @@ static void camd33_auth_client(uchar *camdbug)
 
 static void camd33_send_dcw(struct s_client *UNUSED(client), ECM_REQUEST *er)
 {
-	uchar mbuf[1024];
+	uchar mbuf[128];
 	mbuf[0] = 2;
 	memcpy(mbuf + 1, &er->msgid, 4);  // get pin
 	memcpy(mbuf + 5, er->cw, 16);
@@ -128,11 +128,15 @@ static void camd33_send_dcw(struct s_client *UNUSED(client), ECM_REQUEST *er)
 
 static void camd33_process_ecm(uchar *buf, int32_t l)
 {
-	ECM_REQUEST *er;
+	ECM_REQUEST *er;	
+	if(l < 7)
+		{ return; }
 	if(!(er = get_ecmtask()))
 		{ return; }
 	memcpy(&er->msgid, buf + 3, 4); // save pin
 	er->ecmlen = l - 7;
+	if(er->ecmlen < 0 || er->ecmlen > MAX_ECM_SIZE)
+		{ NULLFREE(er); return; }
 	er->caid = b2i(2, buf + 1);
 	memcpy(er->ecm , buf + 7, er->ecmlen);
 	get_cw(cur_client(), er);
@@ -141,8 +145,12 @@ static void camd33_process_ecm(uchar *buf, int32_t l)
 static void camd33_process_emm(uchar *buf, int32_t l)
 {
 	EMM_PACKET epg;
+	if(l < 7)
+		{ return; }
 	memset(&epg, 0, sizeof(epg));
 	epg.emmlen = l - 7;
+	if(epg.emmlen < 3 || epg.emmlen > MAX_EMM_SIZE)
+		{ return; }
 	memcpy(epg.caid     , buf + 1, 2);
 	memcpy(epg.hexserial, buf + 3, 4);
 	memcpy(epg.emm      , buf + 7, epg.emmlen);

module-camd35.c
@@ -473,18 +473,23 @@ static void camd35_send_dcw(struct s_client *client, ECM_REQUEST *er)
 static void camd35_process_ecm(uchar *buf, int buflen)
 {
 	ECM_REQUEST *er;
+	
 	if(!buf || buflen < 23)
 		{ return; }
-	uint16_t ecmlen = (((buf[21] & 0x0f) << 8) | buf[22]) + 3;
-	if(ecmlen + 20 > buflen)
+		
+	uint16_t ecmlen = SCT_LEN((&buf[20]));
+	
+	if(ecmlen > MAX_ECM_SIZE || ecmlen + 20 > buflen)
 		{ return; }
+	
 	if(!(er = get_ecmtask()))
 		{ return; }
-	//  er->l = buf[1];
-	//fix ECM LEN issue
+
 	er->ecmlen = ecmlen;
+	
 	if(!cs_malloc(&er->src_data, 0x34 + 20 + er->ecmlen))
-		{ return; }
+		{ NULLFREE(er); return; }
+		
 	memcpy(er->src_data, buf, 0x34 + 20 + er->ecmlen);  // save request
 	er->srvid = b2i(2, buf + 8);
 	er->caid = b2i(2, buf + 10);
@@ -501,6 +506,8 @@ static void camd35_process_emm(uchar *buf, int buflen, int emmlen)
 		{ return; }
 	memset(&epg, 0, sizeof(epg));
 	epg.emmlen = emmlen;
+	if(epg.emmlen < 0 || epg.emmlen > MAX_EMM_SIZE)
+		{ return; }
 	memcpy(epg.caid, buf + 10, 2);
 	memcpy(epg.provid, buf + 12 , 4);
 	memcpy(epg.emm, buf + 20, epg.emmlen);
@@ -753,12 +760,14 @@ static int32_t camd35_send_ecm(struct s_client *client, ECM_REQUEST *er)
 
 static int32_t camd35_send_emm(EMM_PACKET *ep)
 {
-	uchar buf[512];
+	uint8_t *buf;
 	struct s_client *cl = cur_client();
-
 
 	if(!camd35_tcp_connect(cl)) { return 0; }
 	cl->reader->card_status = CARD_INSERTED; //for udp
+
+	if(!cs_malloc(&buf, ep->emmlen + 20 + 15))
+	{ return -1; }
 	
 	memset(buf, 0, 20);
 	memset(buf + 20, 0xff, ep->emmlen + 15);
@@ -769,7 +778,10 @@ static int32_t camd35_send_emm(EMM_PACKET *ep)
 	memcpy(buf + 12, ep->provid, 4);
 	memcpy(buf + 20, ep->emm, ep->emmlen);
 
-	return ((camd35_send_without_timeout(cl, buf, 0) < 1) ? 0 : 1);
+	int32_t rc = ((camd35_send_without_timeout(cl, buf, 0) < 1) ? 0 : 1);
+	
+	NULLFREE(buf);
+	return rc;	
 }
 
 static int32_t camd35_recv_chk(struct s_client *client, uchar *dcw, int32_t *rc, uchar *buf, int32_t rc2 __attribute__((unused)))

module-dvbapi-azbox.c
@@ -37,6 +37,9 @@ static void azbox_openxcas_ecm_callback(int32_t stream_id, uint32_t UNUSED(seq),
 	openxcas_ecm_pid = pid;
 	openxcas_busy = 1;
 
+	if(l < 0 || l > MAX_ECM_SIZE)
+		{ return; }
+
 	ECM_REQUEST *er;
 	if(!(er = get_ecmtask()))
 		{ return; }
@@ -70,7 +73,10 @@ static void azbox_openxcas_ex_callback(int32_t stream_id, uint32_t seq, int32_t 
 	openxcas_ecm_pid = pid;
 	openxcas_cipher_idx = idx; // is this really cipher_idx?
 
-	ECM_REQUEST *er;
+	if(l < 0 || l > MAX_ECM_SIZE)
+		{ return; }
+
+	ECM_REQUEST *er;	
 	if(!(er = get_ecmtask()))
 		{ return; }
 

module-dvbapi-mca.c
@@ -320,6 +320,9 @@ static void mca_ecm_callback(int32_t stream_id, uint32_t UNUSED(seq), int32_t ci
 		return;
 	}
 
+	if(l < 0 || l > MAX_ECM_SIZE)
+		{ return; }
+
 	ECM_REQUEST *er;
 	if(!(er = get_ecmtask()))
 		{ return; }
@@ -353,6 +356,9 @@ static void mca_ex_callback(int32_t stream_id, uint32_t UNUSED(seq), int32_t idx
 	openxcas_ecm_pid = pid;
 	openxcas_cipher_idx = idx; // is this really cipher_idx?
 
+	if(l < 0 || l > MAX_ECM_SIZE)
+		{ return; }
+	
 	ECM_REQUEST *er;
 	if(!(er = get_ecmtask()))
 		{ return; }

module-newcamd.c
@@ -1248,14 +1248,25 @@ static void newcamd_process_ecm(struct s_client *cl, uchar *buf, int32_t len)
 {
 	int32_t pi;
 	ECM_REQUEST *er;
+	uint16_t ecmlen;
 
+	if(len < 5)
+		{ return; }
+
+	ecmlen = SCT_LEN((&buf[2]));
+	if(ecmlen < 3 || ecmlen > MAX_ECM_SIZE || ecmlen+2 > len)
+	{
+		return;
+	}
+	
 	if(!(er = get_ecmtask()))
 	{
 		return;
 	}
+	
 	// save client ncd_msgid
 	er->msgid = cl->ncd_msgid;
-	er->ecmlen = (((buf[3] & 0x0F) << 8) | buf[4]) + 3;
+	er->ecmlen = ecmlen;
 	cs_log_dbg(D_CLIENT, "ncd_process_ecm: er->msgid=%d len=%d ecmlen=%d", er->msgid, len, er->ecmlen);
 	er->srvid = cl->ncd_header[4] << 8 | cl->ncd_header[5];
 	er->caid = cl->ncd_header[6] << 8 | cl->ncd_header[7];
@@ -1270,16 +1281,22 @@ static void newcamd_process_ecm(struct s_client *cl, uchar *buf, int32_t len)
 	get_cw(cl, er);
 }
 
-static void newcamd_process_emm(uchar *buf)
+static void newcamd_process_emm(uchar *buf, int32_t len)
 {
 	int32_t ok = 1;
 	uint16_t caid = 0;
 	struct s_client *cl = cur_client();
 	EMM_PACKET epg;
 
+	if(len < 3)
+		{ return; }
+	
 	memset(&epg, 0, sizeof(epg));
 
 	epg.emmlen = buf[2] + 3;
+	if(epg.emmlen > MAX_EMM_SIZE || epg.emmlen > len)
+		{ return; }
+	
 	caid = cfg.ncd_ptab.ports[cl->port_idx].ncd->ncd_ftab.filts[0].caid;
 	epg.caid[0] = (uchar)(caid >> 8);
 	epg.caid[1] = (uchar)(caid);
@@ -1487,7 +1504,7 @@ static void *newcamd_server(struct s_client *client, uchar *mbuf, int32_t len)
 
 	default:
 		if(mbuf[2] > 0x81 && mbuf[2] < 0x90)
-			{ newcamd_process_emm(mbuf + 2); }
+			{ newcamd_process_emm(mbuf + 2, len - 2); }
 		else
 		{
 			cs_log_dbg(D_CLIENT, "unknown newcamd command! (%d)", mbuf[2]);

module-pandora.c
@@ -31,6 +31,9 @@ static void pandora_process_request(struct s_client *cl, uchar *buf, int32_t l)
 	ECM_REQUEST *er;
 	uchar md5tmp[MD5_DIGEST_LENGTH];
 
+	if(l < 10 + CS_ECMSTORESIZE + 2)
+		{ return; }
+	
 	if(!(er = get_ecmtask()))
 		{ return; }
 	er->caid = b2i(2, buf + 1);
@@ -39,11 +42,16 @@ static void pandora_process_request(struct s_client *cl, uchar *buf, int32_t l)
 	//er->ecmcrc32 = crc32(0L, buf+10, CS_ECMSTORESIZE);
 	er->chid = b2i(2, buf + 10 + CS_ECMSTORESIZE);
 
-	if(l > 12 + CS_ECMSTORESIZE + 16)
+	if(!cl->pand_ignore_ecm && (l >= 10 + CS_ECMSTORESIZE + 2 + 2))
 	{
-		ecmlen = b2i(2, buf + 12 + CS_ECMSTORESIZE);
-		if((ecmlen > 320) || cl->pand_ignore_ecm)
-			{ er->ecmlen = 0; }
+		ecmlen = b2i(2, buf + 10 + CS_ECMSTORESIZE + 2);
+			
+		if(ecmlen < 0 || ecmlen > MAX_ECM_SIZE
+			|| ((10 + CS_ECMSTORESIZE + 2 + 2 + ecmlen) > CWS_NETMSGSIZE)
+			|| ((10 + CS_ECMSTORESIZE + 2 + 2 + ecmlen) > l))
+		{
+			er->ecmlen = 0;
+		}
 		else
 		{
 			if(!memcmp(buf + 10,
@@ -273,6 +281,8 @@ static int pandora_send_ecm(struct s_client *cl, ECM_REQUEST *er)
 	len = 12 + CS_ECMSTORESIZE;
 	if(cl->pand_send_ecm)
 	{
+		if(len+2+er->ecmlen > CWS_NETMSGSIZE)
+			{ return -1; }
 		msgbuf[12 + CS_ECMSTORESIZE] = er->ecmlen >> 8;
 		msgbuf[13 + CS_ECMSTORESIZE] = er->ecmlen & 0xFF;
 		memcpy(&msgbuf[14 + CS_ECMSTORESIZE], er->ecm, er->ecmlen);

commit 0a958a5892d38c0f4fe6c8e008a732939e277e76

fixed cccam problem introduced in r10812
git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@10816 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-cccam.c
@@ -2567,9 +2567,7 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 			if((l >= (4+8)) && strncmp(msg, "PARTNER:", 8) == 0)
 			{
 				//When Data starts with "PARTNER:" we have an Oscam-cccam-compatible client/server!
-				if((int32_t)(4 + 9 + sizeof(cc->remote_oscam) - 1) > l)
-					{ break; }
-				
+
 				strncpy(cc->remote_oscam, msg + 9, sizeof(cc->remote_oscam) - 1);
 				int32_t has_param = check_extended_mode(cl, msg);
 				if(!cc->is_oscam_cccam)
@@ -3886,7 +3884,7 @@ int32_t cc_cli_connect(struct s_client *cl)
 		return -2;
 	}
 	cc_crypt(&cc->block[DECRYPT], data, 20, DECRYPT);
-	cs_log_dump_dbg(D_CLIENT, data, 20, "cccam: login failed, usr/pwd invalid");
+	cs_log_dump_dbg(D_CLIENT, data, 20, "cccam: login data");
 
 	if(memcmp(data, buf, 5))    // check server response
 	{