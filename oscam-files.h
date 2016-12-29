0f3b090109aef00e510d8492fdac6b79ff4529ef
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2016-12-29 18:50:43 +0000

    revert #11294
    wrong patch
    
    git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11295 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-files.h
+++ oscam-files.h
@@ -7,8 +7,5 @@ int32_t cs_readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result);
 bool file_exists(const char *filename);
 int32_t file_copy(char *srcfile, char *destfile);
 int32_t safe_overwrite_with_bak(char *destfile, char *temp_file, char *bakfile, int32_t forceBakOverWrite);
-#ifdef MODULE_GBOX
-char *get_gbox_filename(char *dest, size_t destlen, const char *filename);
-#endif
 
 #endif
