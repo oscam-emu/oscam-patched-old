oscam-emu/oscam-patched-old

commit 81599326472c145f77dfeca473a051d2574dd21d

fix warning in oscam-ecm.c macosx build
git-svn-id: https://svn.streamboard.tv/oscam/trunk@11323 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam-ecm.c
@@ -538,7 +538,7 @@ ECM_REQUEST *get_ecmtask(void)
 
 void cleanup_ecmtasks(struct s_client *cl)
 {
-	if(cl && !cl->account->usr) { return; }  //not for anonymous users!
+	if(cl && !*cl->account->usr) { return; }  //not for anonymous users!
 
 	ECM_REQUEST *ecm;
 
@@ -1688,7 +1688,7 @@ int32_t write_ecm_answer(struct s_reader *reader, ECM_REQUEST *er, int8_t rc, ui
 	if(!ea->is_pending)   //not for pending ea - only once for ea
 	{
 		//cache update
-		if(ea && ea->rc < E_NOTFOUND && ea->cw)
+		if(ea && (ea->rc < E_NOTFOUND) && *ea->cw)
 			add_cache_from_reader(er, reader, er->csp_hash, er->ecmd5, ea->cw, er->caid, er->prid, er->srvid );
 
 		//readers stats for LB

commit 067d6bc5e3537baa05c6c7e369dcf979b60b603b

- Fix regression introduced in r11323
git-svn-id: https://svn.streamboard.tv/oscam/trunk@11343 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam-ecm.c
@@ -1691,7 +1691,7 @@ int32_t write_ecm_answer(struct s_reader *reader, ECM_REQUEST *er, int8_t rc, ui
 	if(!ea->is_pending)   //not for pending ea - only once for ea
 	{
 		//cache update
-		if(ea && (ea->rc < E_NOTFOUND) && *ea->cw)
+		if(ea && (ea->rc < E_NOTFOUND) && (ea->cw))
 			add_cache_from_reader(er, reader, er->csp_hash, er->ecmd5, ea->cw, er->caid, er->prid, er->srvid );
 
 		//readers stats for LB