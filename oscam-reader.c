6d7dbd110cffcba54a49e93d5401f7d6ac618d32
felixka <felixka@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2017-02-28 18:33:21 +0000

    - revert changeset 11364/5 - nobody should ask me again to release patches that are not proved by several users
    git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11366 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-reader.c
+++ oscam-reader.c
@@ -504,7 +504,11 @@ const char *reader_get_type_desc(struct s_reader *rdr, int32_t extended)
 	}
 	if(rdr->typ == R_NEWCAMD && rdr->ncd_proto == NCD_524)
 		{ desc = "newcamd524"; }
-	else if(rdr->typ == R_CCCAM && cccam_client_extended_mode(rdr->client) && !cccam_client_multics_mode(rdr->client))
+	else if(extended && rdr->typ == R_CCCAM && cccam_client_extended_mode(rdr->client) && !cccam_client_multics_mode(rdr->client))
+	{
+		desc = "cccam_ext";
+	}
+	else if(!extended && rdr->typ == R_CCCAM && cccam_client_extended_mode(rdr->client) && !cccam_client_multics_mode(rdr->client))
 	{
 		desc = "cccam_ext";
 	}
