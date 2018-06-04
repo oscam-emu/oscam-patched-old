38bf44bdca5a58847aff0d7fc1289ae73d416813
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2018-06-04 19:27:07 +0000

    fix wrong bufsize in module-webif.c
    git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11424 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-webif.c
+++ module-webif.c
@@ -6685,7 +6685,7 @@ static bool process_single_emm(struct templatevars * vars, struct s_reader * rdr
 	{
 		char emmdata[1025] = {'\0'};     /*1024 + '\0'*/
 		unsigned char emmhex[513] = {'\0'};
-		char buff[5] = {'\0'};
+		char buff[7] = {'\0'};
 		uint16_t len = 0;
 		cs_strncpy(emmdata, ep, sizeof(emmdata));
 		remove_white_chars(emmdata);
