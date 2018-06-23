5e27065078b3dc44db6ab3653bc004cd7a597bf5
theparasol <theparasol@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2018-06-23 21:06:48 +0000

    Improved oscam revision handling for git
    git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11425 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- config.sh
+++ config.sh
@@ -692,7 +692,7 @@ do
 		revision=`(svnversion -n . 2>/dev/null || printf 0) | sed 's/.*://; s/[^0-9]*$//; s/^$/0/'`
 		if [ "$revision" = "0" ] 
 		then
-			which git > /dev/null 2>&1 && revision=`git log -1 --pretty=%B | grep git-svn-id | sed -n -e 's/^.*trunk@\([0-9]*\) .*$/\1/p'`
+			which git > /dev/null 2>&1 && revision=`git log -10 --pretty=%B | grep git-svn-id | head -n 1 | sed -n -e 's/^.*trunk@\([0-9]*\) .*$/\1/p'`
 		fi
 		echo $revision
 		break
