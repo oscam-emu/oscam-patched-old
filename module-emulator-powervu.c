245959cb347890b33c1d104818f0a12091abbbb1
oscam-emu <oscam.emu@gmail.com>
2018-10-22 16:04:04 +0300

    [powervu] Move #defines to header file

--- module-emulator-powervu.c
+++ module-emulator-powervu.c
@@ -8,30 +8,13 @@
 #include "ffdecsa/ffdecsa.h"
 #include "module-emulator-osemu.h"
 #include "module-emulator-streamserver.h"
+#include "module-emulator-powervu.h"
 #include "oscam-string.h"
 #include "oscam-time.h"
 
 #if defined(__ANDROID__)
 #define __bswap_32 bswap_32
 #endif
-
-#define PVU_CW_VID 0	// VIDeo
-#define PVU_CW_HSD 1	// High Speed Data
-#define PVU_CW_A1 2	// Audio 1
-#define PVU_CW_A2 3	// Audio 2
-#define PVU_CW_A3 4	// Audio 3
-#define PVU_CW_A4 5	// Audio 4
-#define PVU_CW_UTL 6	// UTiLity
-#define PVU_CW_VBI 7	// Vertical Blanking Interval
-
-#define PVU_CONVCW_VID_ECM 0x80	// VIDeo
-#define PVU_CONVCW_HSD_ECM 0x40	// High Speed Data
-#define PVU_CONVCW_A1_ECM 0x20	// Audio 1
-#define PVU_CONVCW_A2_ECM 0x10	// Audio 2
-#define PVU_CONVCW_A3_ECM 0x08	// Audio 3
-#define PVU_CONVCW_A4_ECM 0x04	// Audio 4
-#define PVU_CONVCW_UTL_ECM 0x02	// UTiLity
-#define PVU_CONVCW_VBI_ECM 0x01	// Vertical Blanking Interval
 
 static inline uint8_t GetBit(uint8_t byte, uint8_t bitnb)
 {
--- module-emulator-powervu.h
+++ module-emulator-powervu.h
@@ -3,14 +3,23 @@
 
 #ifdef WITH_EMU
 
-#define PVU_CW_VID 0    // VIDeo
-#define PVU_CW_HSD 1    // High Speed Data
-#define PVU_CW_A1  2    // Audio 1
-#define PVU_CW_A2  3    // Audio 2
-#define PVU_CW_A3  4    // Audio 3
-#define PVU_CW_A4  5    // Audio 4
-#define PVU_CW_UTL 6    // UTiLity
-#define PVU_CW_VBI 7    // Vertical Blanking Interval
+#define PVU_CW_VID	0	// VIDeo
+#define PVU_CW_HSD	1	// High Speed Data
+#define PVU_CW_A1	2	// Audio 1
+#define PVU_CW_A2	3	// Audio 2
+#define PVU_CW_A3	4	// Audio 3
+#define PVU_CW_A4	5	// Audio 4
+#define PVU_CW_UTL	6	// UTiLity
+#define PVU_CW_VBI	7	// Vertical Blanking Interval
+
+#define PVU_CONVCW_VID_ECM	0x80	// VIDeo
+#define PVU_CONVCW_HSD_ECM	0x40	// High Speed Data
+#define PVU_CONVCW_A1_ECM	0x20	// Audio 1
+#define PVU_CONVCW_A2_ECM	0x10	// Audio 2
+#define PVU_CONVCW_A3_ECM	0x08	// Audio 3
+#define PVU_CONVCW_A4_ECM	0x04	// Audio 4
+#define PVU_CONVCW_UTL_ECM	0x02	// UTiLity
+#define PVU_CONVCW_VBI_ECM	0x01	// Vertical Blanking Interval
 
 int8_t PowervuECM(uint8_t *ecm, uint8_t *dw, uint16_t srvid, emu_stream_client_key_data *cdata, EXTENDED_CW* cw_ex);
 int8_t PowervuEMM(uint8_t *emm, uint32_t *keysAdded);
