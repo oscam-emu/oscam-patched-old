0912ceade1880466938c7d7064cde7c7fef14a92
oscam-emu <oscam.emu@gmail.com>
2018-11-07 19:46:53 +0200

    [osemu] Bump version to 776

--- module-emulator.c
+++ module-emulator.c
@@ -46,7 +46,7 @@ pthread_mutex_t emu_key_data_mutex;
 
 static inline uint32_t GetOSemuVersion(void)
 {
-	return atoi("$Version: 775 $" + 10);
+	return atoi("$Version: 776 $" + 10);
 }
 
 static void set_hexserial_to_version(struct s_reader *rdr)
