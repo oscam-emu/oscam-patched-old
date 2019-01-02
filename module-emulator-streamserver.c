0f94d5f171741033a08add1f3fb2034299621814
oscam-emu <oscam.emu@gmail.com>
2019-01-02 22:51:42 +0200

    [streamserver] Added support for Compel encryption system This unconventional encryption system (data are XOR-ed with a dynamic key) works only via the Stream Relay option. Refer to the wiki for more information on how to configure OSCam-Emu for use with Compel.

--- module-emulator-streamserver.c
+++ module-emulator-streamserver.c
@@ -670,6 +670,168 @@ static void DescrambleTsPacketsPowervu(emu_stream_client_data *data, uint8_t *st
 	}
 }
 
+static void DescrambleTsPacketsCompel(emu_stream_client_data *data, uint8_t *stream_buf, uint32_t bufLength, uint16_t packetSize)
+{
+	int8_t can_decode = 0;
+	int32_t j;
+
+	uint8_t scramblingControl;
+	uint16_t pid, offset;
+	uint32_t i, tsHeader;
+
+	for (i = 0; i < bufLength; i += packetSize)
+	{
+		tsHeader = b2i(4, stream_buf + i);
+		pid = (tsHeader & 0x1FFF00) >> 8;
+		scramblingControl = tsHeader & 0xC0;
+
+		if (tsHeader & 0x20)
+		{
+			offset = 4 + stream_buf[i + 4] + 1;
+		}
+		else
+		{
+			offset = 4;
+		}
+
+		if (packetSize - offset < 1)
+			{ continue; }
+
+		if (scramblingControl == 0)
+			{ continue; }
+
+		if (!(stream_buf[i + 3] & 0x10))
+		{
+			stream_buf[i + 3] &= 0x3F;
+			continue;
+		}
+
+		if (pid == data->video_pid)
+		{
+			can_decode = 1;
+		}
+		else
+		{
+			for (j = 0; j < data->audio_pid_count; j++)
+			{
+					if (pid == data->audio_pids[j])
+					{
+						can_decode = 1;
+						break;
+					}
+			}
+		}
+
+		if (can_decode)
+		{
+			static uint8_t dyn_key[184];
+
+			int8_t matches00 = 0;
+			int8_t matchesFF = 0;
+			int8_t last00_was_good = 0;
+			int8_t lastFF_was_good = 0;
+			int8_t limit = 64;
+
+			for (j = 0; j < 184; j++)
+			{
+				if (stream_buf[i + 4 + j] == 0x00)
+				{
+					last00_was_good = 1;
+					matches00++;
+					if (matches00 > limit) dyn_key[j] = 0x00;
+				}
+				else if (stream_buf[i + 4 + j] == 0x3F)
+				{
+					last00_was_good = 1;
+					matches00++;
+					if (matches00 > limit) dyn_key[j] = 0x3F;
+				}
+				else
+				{
+					if (last00_was_good) matches00--;
+					else matches00 -= 2;
+
+					if (matches00 < 0) matches00 = 0;
+					last00_was_good = 0;
+				}
+
+				if (stream_buf[i + 4 + j] == 0xC0)
+				{
+					lastFF_was_good = 1;
+					matchesFF++;
+					if (matchesFF > limit) dyn_key[j] = 0x3F;
+				}
+				else if (stream_buf[i + 4 + j] == 0xFF)
+				{
+					lastFF_was_good = 1;
+					matchesFF++;
+					if (matchesFF > limit) dyn_key[j] = 0x00;
+				}
+				else
+				{
+					if (lastFF_was_good) matchesFF--;
+					else matchesFF -= 2;
+
+					if (matchesFF < 0) matchesFF = 0;
+					lastFF_was_good = 0;
+				}
+			}
+
+			for (j = 183; j >= 0; j--)
+			{
+				if (stream_buf[i + 4 + j] == 0x00)
+				{
+					last00_was_good = 1;
+					matches00++;
+					if (matches00 > limit) dyn_key[j] = 0x00;
+				}
+				else if (stream_buf[i + 4 + j] == 0x3F)
+				{
+					last00_was_good = 1;
+					matches00++;
+					if (matches00 > limit) dyn_key[j] = 0x3F;
+				}
+				else
+				{
+					if (last00_was_good) matches00--;
+					else matches00 -= 2;
+
+					if (matches00 < 0) matches00 = 0;
+					last00_was_good = 0;
+				}
+
+				if (stream_buf[i + 4 + j] == 0xC0)
+				{
+					lastFF_was_good = 1;
+					matchesFF++;
+					if (matchesFF > limit) dyn_key[j] = 0x3F;
+				}
+				else if (stream_buf[i + 4 + j] == 0xFF)
+				{
+					lastFF_was_good = 1;
+					matchesFF++;
+					if (matchesFF > limit) dyn_key[j] = 0x00;
+				}
+				else
+				{
+					if (lastFF_was_good) matchesFF--;
+					else matchesFF -= 2;
+
+					if (matchesFF < 0) matchesFF = 0;
+					lastFF_was_good = 0;
+				}
+			}
+
+			for (j = 0; j < 184; j++)
+			{
+				stream_buf[i + 4 + j] ^= dyn_key[j];
+			}
+
+			stream_buf[i + 3] &= 0x3F;
+		}
+	}
+}
+
 static int32_t connect_to_stream(char *http_buf, int32_t http_buf_len, char *stream_path)
 {
 	struct sockaddr_in cservaddr;
@@ -960,6 +1122,10 @@ static void *stream_client_handler(void *arg)
 							if (caid_is_powervu(data->caid))
 							{
 								DescrambleTsPacketsPowervu(data, stream_buf + startOffset, packetCount * packetSize, packetSize);
+							}
+							else if (data->caid == NO_CAID_VALUE) // Compel
+							{
+								DescrambleTsPacketsCompel(data, stream_buf + startOffset, packetCount * packetSize, packetSize);
 							}
 						}
 						else
