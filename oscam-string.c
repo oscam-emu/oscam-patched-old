e35e5515e466040b73da46027b9bd72c4a1e9a5a
gorgone.impertinence <3585-gorgone.impertinance@users.noreply.git.streamboard.tv>
2019-01-11 20:18:03 +0000

    fix typo missing ;
    git-svn-id: https://svn.streamboard.tv/oscam/trunk@11459 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- oscam-string.c
+++ oscam-string.c
@@ -305,7 +305,7 @@ uchar *i2b_buf(int32_t n, uint32_t i, uchar *b)
 		b[0] = (i >> 16) & 0xff;
 		b[1] = (i >> 8) & 0xff;
 		b[2] = (i) & 0xff;
-		break
+		break;
 	case 4:
 		b[0] = (i >> 24) & 0xff;
 		b[1] = (i >> 16) & 0xff;
