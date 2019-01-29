43d1a5909b684ed33610b697e54c4091937af7cd
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2019-01-29 16:35:10 +0000

    some cleanups for newcamd and add script for git svn diff creation
    git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11489 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-newcamd-des.h
+++ module-newcamd-des.h
@@ -1,8 +1,17 @@
 #ifndef MODULE_NEWCAMD_DES_H_
 #define MODULE_NEWCAMD_DES_H_
 
-	int nc_des_encrypt(uint8_t *buffer, int len, uint8_t *deskey);
-	int nc_des_decrypt(uint8_t *buffer, int len, uint8_t *deskey);
-	uint8_t *nc_des_login_key_get(uint8_t *key1, uint8_t *key2, int len, uint8_t *des16);
+#define DES_IP    1
+#define DES_IP_1  2
+#define DES_RIGHT 4
+#define DES_HASH  8
+
+#define DES_ECM_CRYPT    0
+#define DES_ECM_HASH     DES_HASH
+
+void nc_des(uint8_t key[], uint8_t mode, uint8_t data[])
+int nc_des_encrypt(uint8_t *buffer, int len, uint8_t *deskey);
+int nc_des_decrypt(uint8_t *buffer, int len, uint8_t *deskey);
+uint8_t *nc_des_login_key_get(uint8_t *key1, uint8_t *key2, int len, uint8_t *des16);
 
 #endif
