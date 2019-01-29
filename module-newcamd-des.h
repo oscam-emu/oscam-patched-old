1851a012fb050f2f9429750bae8815a1c8ae8a66
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2019-01-29 17:13:08 +0000

    fix last commit missing ;
    git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11490 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-newcamd-des.h
+++ module-newcamd-des.h
@@ -9,7 +9,7 @@
 #define DES_ECM_CRYPT    0
 #define DES_ECM_HASH     DES_HASH
 
-void nc_des(uint8_t key[], uint8_t mode, uint8_t data[])
+void nc_des(uint8_t key[], uint8_t mode, uint8_t data[]);
 int nc_des_encrypt(uint8_t *buffer, int len, uint8_t *deskey);
 int nc_des_decrypt(uint8_t *buffer, int len, uint8_t *deskey);
 uint8_t *nc_des_login_key_get(uint8_t *key1, uint8_t *key2, int len, uint8_t *des16);
