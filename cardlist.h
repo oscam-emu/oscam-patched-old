21974c971887b910a022ee1d339fef33467de38a
gorgone.impertinence <gorgone.impertinence@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2019-02-15 16:57:25 +0000

    complete remove of cardlist.c + cardlist.h
    git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11501 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- cardlist.h
+++
@@ -1,5 +0,0 @@
-#ifndef _CSCTAPI_CARDLIST_H_
-#define _CSCTAPI_CARDLIST_H_
-struct atrlist{ int found; int ishd03; int badcard; int ishd04; char providername[32]; char atr[80]; };
-void findatr(struct s_reader *reader);
-#endif
