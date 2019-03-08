85207a6ffa04b86fa181e05fa5fd22214f50431c
oscam-emu <oscam.emu@gmail.com>
2019-03-08 18:26:07 +0200

    [powervu] Added support for hash mode 31

--- module-emulator-powervu.c
+++ module-emulator-powervu.c
@@ -457,6 +457,7 @@ static const uint8_t table1B[] = { 0x03, 0x07, 0x08, 0x10 };
 static const uint8_t table1C[] = { 0x03, 0x05, 0x0A, 0x10 };
 static const uint8_t table1D[] = { 0x03, 0x07, 0x0A, 0x10 };
 static const uint8_t table1E[] = { 0x01, 0x05, 0x0B, 0x10 };
+static const uint8_t table1F[] = { 0x06, 0x07, 0x0B, 0x10 };
 
 static void hash_modes_19_to_27_tables_3(uint8_t *data, uint8_t *hash, const uint8_t *table)
 {
@@ -619,6 +620,10 @@ static void create_hash(uint8_t *data, int len, uint8_t *hash, int mode)
 
 		case 30:
 			hash_modes_19_to_27_tables_3(dataPadded, hash, table1E);
+			break;
+
+		case 31:
+			hash_modes_19_to_27_tables_3(dataPadded, hash, table1F);
 			break;
 
 		default:
