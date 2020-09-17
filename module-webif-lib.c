2c7a3ea054a9fd00e771852d248c9f64c1317b25
gorgone.impertinence <3585-gorgone.impertinance@users.noreply.git.streamboard.tv>
2020-09-17 15:45:25 +0000

    safer protocol implementation in oscam to prevent buffer overflow
    #4813
    
    git-svn-id: https://svn.streamboard.tv/oscam/trunk@11612 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-webif-lib.c
+++ module-webif-lib.c
@@ -639,10 +639,15 @@ int8_t get_stats_linux(const pid_t pid, struct pstat* result)
 	// convert pid to string
 	char pid_s[20];
 	snprintf(pid_s, sizeof(pid_s), "%d", pid);
-	char stat_filepath[30] = "/proc/"; strncat(stat_filepath, pid_s,
-			sizeof(stat_filepath) - strlen(stat_filepath) -1);
-	strncat(stat_filepath, "/stat", sizeof(stat_filepath) -
-			strlen(stat_filepath) -1);
+	char stat_filepath[30] = "/proc/";
+
+	if (!cs_strncat(stat_filepath, pid_s, sizeof(stat_filepath))) {
+		return -1;
+	}
+
+	if (!cs_strncat(stat_filepath, "/stat", sizeof(stat_filepath))) {
+		return -1;
+	}
 
 	FILE *f_pstat = fopen(stat_filepath, "r");
 	if (f_pstat == NULL) {
