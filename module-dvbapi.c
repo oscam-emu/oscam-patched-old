a34b3e6f370850c04215238358ecefd32b7ed20b
nautilus7 <4275-nautilus7@users.noreply.git.streamboard.tv>
2022-05-08 17:57:22 +0000

    [dvbapi] Only process valid CAIDS and ECM / EMM pids
    CAID 0x0000 is not valid (reserved by EBU)
    Pid 0x1FFF is reserved for NULL packets.
    
    git-svn-id: https://svn.streamboard.tv/oscam/trunk@11706 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-dvbapi.c
+++ module-dvbapi.c
@@ -2132,6 +2132,11 @@ static void dvbapi_parse_cat_ca_descriptor(int32_t demux_id, const uint8_t *buff
 	ca_system_id = b2i(2, buffer);
 	ca_pid = b2i(2, buffer + 2) & 0x1FFF;
 
+	if (ca_system_id == 0x0000 || ca_pid == 0x1FFF)
+	{
+		return; // This is not a valid CAID or EMM pid
+	}
+
 	switch(ca_system_id >> 8)
 	{
 		case 0x01:
@@ -3837,6 +3842,11 @@ static void dvbapi_parse_pmt_ca_descriptor(int32_t demux_id, const uint8_t *buff
 
 	ca_system_id = b2i(2, buffer);
 	ca_pid = b2i(2, buffer + 2) & 0x1FFF;
+
+	if (ca_system_id == 0x0000 || ca_pid == 0x1FFF)
+	{
+		return; // This is not a valid CAID or ECM pid
+	}
 
 	if(caid_is_seca(ca_system_id))
 	{
