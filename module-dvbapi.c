0876c31eff089db5d0e46d03db1d5783a93da7e3
nautilus7 <nautilus7@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2022-05-10 20:42:00 +0000

    [dvbapi] Allow ECM / EMM pid 0x1FFF only for BISS
    git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@11709 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-dvbapi.c
+++ module-dvbapi.c
@@ -2132,9 +2132,9 @@ static void dvbapi_parse_cat_ca_descriptor(int32_t demux_id, const uint8_t *buff
 	ca_system_id = b2i(2, buffer);
 	ca_pid = b2i(2, buffer + 2) & 0x1FFF;
 
-	if(ca_system_id == 0x0000)
+	if(ca_system_id == 0x0000 || (!caid_is_biss_fixed(ca_system_id) && ca_pid == 0x1FFF))
 	{
-		return; // This is not a valid CAID
+		return; // This is not a valid CAID or EMM pid
 	}
 
 	switch(ca_system_id >> 8)
@@ -3843,9 +3843,9 @@ static void dvbapi_parse_pmt_ca_descriptor(int32_t demux_id, const uint8_t *buff
 	ca_system_id = b2i(2, buffer);
 	ca_pid = b2i(2, buffer + 2) & 0x1FFF;
 
-	if(ca_system_id == 0x0000)
+	if(ca_system_id == 0x0000 || (!caid_is_biss_fixed(ca_system_id) && ca_pid == 0x1FFF))
 	{
-		return; // This is not a valid CAID
+		return; // This is not a valid CAID or ECM pid
 	}
 
 	if(caid_is_seca(ca_system_id))
