99c4631a73e3da426a6d31557f64fef610de183d
oscam-emu <oscam.emu@gmail.com>
2024-03-14 00:01:11 +0200

    Revert "Add icam DE v9"
    This reverts commit 997b30bbb597cb4e7bc7dbe6ce8e34a17f85e914.

--- module-dvbapi.c
+++ module-dvbapi.c
@@ -11,7 +11,6 @@
 #include "module-dvbapi-coolapi.h"
 #include "module-dvbapi-stapi.h"
 #include "module-dvbapi-chancache.h"
-#include "module-emulator-streamserver.h"
 #include "module-stat.h"
 #include "oscam-chk.h"
 #include "oscam-client.h"
@@ -7851,13 +7850,7 @@ void dvbapi_send_dcw(struct s_client *client, ECM_REQUEST *er)
 		delayer(er, delay);
 
 #ifdef WITH_EMU
-		bool set_dvbapi_cw = true;
-		if(chk_ctab_ex(er->caid, &cfg.emu_stream_relay_ctab) && cfg.emu_stream_relay_enabled)
-		{
-			// streamserver set cw
-			set_dvbapi_cw = !stream_write_cw(er);
-		}
-		if(set_dvbapi_cw)
+		if(!chk_ctab_ex(er->caid, &cfg.emu_stream_relay_ctab) || !cfg.emu_stream_relay_enabled)
 #endif
 		switch(selected_api)
 		{
